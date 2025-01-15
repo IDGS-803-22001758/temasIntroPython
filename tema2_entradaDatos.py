60e58a8a677c69dd9497a437ad33d7f69e98224f
IDGS-803-22001758 <79677@alumnos.utleon.edu.mx>
2025-01-14 23:44:31 -0600

    Temas de introduccion y ejercicio While

---
+++ tema2_entradaDatos.py
@@ -0,0 +1,6 @@
+
+print("Dame dos numeros")
+num1=input('Numero1: ')
+num2=input('Numero2: ')
+res=int(num1)+int(num2)
+print("La suma de {0} + {1} = {2}".format(num1,num2,res))
---
+++ tema4_while.py
@@ -0,0 +1,8 @@
+
+x=1
+numero=int(input('Ingresa el numero de la tabla: '))
+
+while x<=10:
+    res=numero*x
+    print(res)
+    x=x+1
